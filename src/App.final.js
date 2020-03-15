9eaa33b0c7ade6be814a5e1289549e8a8545fd80
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-03-15 09:49:08 -0700

    push after npm audit fixes

--- src/App.final.js
+++ src/App.final.js
@@ -2,56 +2,53 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  // use destructuring
+// class App extends React.Component {
+  function App(){
   const [currentTimes, setCurrentTimes] = useState([]);
   const handleAdd = () => {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    const milliseconds = date.getMilliseconds();
-    const now = `${hours}:${minutes}:${seconds}:${milliseconds}`
-    // console.log(now);
+    const millisecs = date.getMilliseconds();
+    const now = `${hours}:${minutes}:${seconds}`
+    
     // copy our array
     const newCurrentTimes = [...currentTimes];
-    // push our time to the array
+    // push our time to our array
     newCurrentTimes.push(now);
     setCurrentTimes(newCurrentTimes);
   };
-  const handleDelete= (index) => {
-    // delete the index from currentTimes
-    // copy our array
+  const handleDelete = (index) => {
     const newCurrentTimes = [...currentTimes];
     newCurrentTimes.splice(index, 1);
-    // update currentTimes
     setCurrentTimes(newCurrentTimes);
   };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" className="App-logo"/>
-        <button onClick={handleAdd}>+</button>
-        <ul>
-          {currentTimes.map((currentTime, index) => (
-            <li key={index}>
-            {currentTime}
-            <button onClick={() => handleDelete(index)}>-</button>
-            </li>
-          ))}
-        </ul>
-      </header>
-    </div>
-  );
+  // render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={handleAdd}>+</button>
+          <ul>
+            {currentTimes.map((currentTime, index) => (
+              <li className="listitems" key={index}>
+                {currentTime}
+                <button onClick={() => handleDelete(index)}>-</button>
+              </li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  // }
 }
 
-export default App;
-
 // As a user, I want to click a button to show the current time
-  // render a button to the screen
-    // create a handler function to add the time
-      // create the time
-        // add to our storage container
+// render a button to the screen
+// create a handler function to add the time
+// create the time
+// add to our storage container
 
 // As a user, I want to click the button more than once, and
 // display each current time on click in a list
@@ -107,5 +104,5 @@ export default App;
 //   );
 // }
 
-// export default App;
+export default App;
 
